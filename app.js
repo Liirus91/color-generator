233779d529b7144761053e5f471e7fa121c48216
Liirus91 <work.liirus@gmail.com>
2023-05-26 10:21:12 +0300

    feat: added changing the color of the text depending on the lightness of the color

--- app.js
+++ app.js
@@ -11,14 +11,40 @@ function generateRandomColor() {
   return color;
 }
 
+function isLight(color) {
+  const r = parseInt(color.substring(0, 2), 16) / 255;
+  const g = parseInt(color.substring(2, 4), 16) / 255;
+  const b = parseInt(color.substring(4, 6), 16) / 255;
+
+  const max = Math.max(r, g, b);
+  const min = Math.min(r, g, b);
+  let l = (max + min) / 2;
+
+  return l >= 0.5 ? true : false;
+}
+
 function setRandomColors() {
   columns.forEach((col) => {
     const color = generateRandomColor();
     const text = col.querySelector('h2');
+    const button = col.querySelector('button');
 
     text.textContent = color;
     col.style.background = color;
+
+    setTextColor(text, color);
+    setTextColor(button, color);
   });
+}
+
+function setTextColor(text, color) {
+  const colorIsLight = isLight(color.substring(1));
+  let textColor = '#FFFFFF';
+  if (colorIsLight) {
+    textColor = '#000000';
+  }
+
+  text.style.color = textColor;
 }
 
 setRandomColors();
